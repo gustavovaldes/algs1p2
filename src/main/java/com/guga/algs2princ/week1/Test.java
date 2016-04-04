470d6d62c05932bd38bd188ad30b211356651f93
Gustavo Valdes <gvaldes@gustavo-nisum.local>
2016-04-04 10:51:12 -0300

    Initial commit

---
+++ src/main/java/com/guga/algs2princ/week1/Test.java
@@ -0,0 +1,11 @@
+package com.guga.algs2princ.week1;
+
+/**
+ * Created by gvaldes
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+    }
+}
