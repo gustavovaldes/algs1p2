824db1074d2467635cc5674cef823ce11a0c7137
gustavovaldes <g.valdes.aracena@gmail.com>
2016-04-05 23:58:48 -0300

    BFS, test pending

---
+++ src/main/java/com/guga/algs2princ/week1/BreadthFirstSearch.java
@@ -0,0 +1,32 @@
+package com.guga.algs2princ.week1;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by guga
+ */
+public class BreadthFirstSearch {
+
+    private boolean[] marked;
+    private int[] edgeTo;
+
+    private void bfs(Graph G, int v) {
+        Deque<Integer> queue = new LinkedList<Integer>();
+        marked[v] = true;
+        queue.add(v);
+        while (queue.size() > 0) {
+            int node = queue.poll();
+            for (Integer w : G.adj(node)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = node;
+                    queue.push(w);
+                }
+            }
+        }
+
+    }
+}
--- src/main/java/com/guga/algs2princ/week1/Test.java
+++ src/main/java/com/guga/algs2princ/week1/Test.java
@@ -7,5 +7,10 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("ok");
+
+        String needle = "holax";
+        String haystack = "sdsholasd";
+        System.out.println(haystack.indexOf(needle));
+
     }
 }
