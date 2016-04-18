139593a0d61da170edbdb8da4a72ea557edc0c6b
gustavovaldes <g.valdes.aracena@gmail.com>
2016-04-18 01:13:02 -0300

    SAP passing, adding test resources and Outcase & WordNet APIs

---
+++ src/main/java/com/guga/algs2princ/week1/Outcast.java
@@ -0,0 +1,18 @@
+package com.guga.algs2princ.week1;
+
+/**
+ * Created by guga
+ */
+public class Outcast {
+    public Outcast(WordNet wordnet) {
+
+    }         // constructor takes a WordNet object
+
+    public String outcast(String[] nouns) {
+        return null;
+    }   // given an array of WordNet nouns, return an outcast
+
+    public static void main(String[] args) {
+
+    }  // see test client below
+}
--- src/main/java/com/guga/algs2princ/week1/SAP.java
+++ src/main/java/com/guga/algs2princ/week1/SAP.java
@@ -1,5 +1,6 @@
 package com.guga.algs2princ.week1;
 
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 
 /**
@@ -7,33 +8,89 @@ import edu.princeton.cs.algs4.Digraph;
  */
 public class SAP {
 
-    // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph G){
+    private Digraph G;
 
+    // constructor takes a digraph (not necessarily a DAG)
+    public SAP(Digraph G) {
+        this.G = new Digraph(G);
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w){
-        return -1;
+    public int length(int v, int w) {
+        BreadthFirstDirectedPaths bfdV = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths bfdW = new BreadthFirstDirectedPaths(G, w);
+        int distance = Integer.MAX_VALUE;
+        int commonAncestor = -1;
+        for (int i = 0; i < G.V(); i++) {
+            if (bfdV.hasPathTo(i) && bfdW.hasPathTo(i)) {
+                int local = bfdV.distTo(i) + bfdW.distTo(i);
+                if (local < distance) {
+                    distance = local;
+                    commonAncestor = i;
+                }
+            }
+        }
+        if (commonAncestor == -1) return commonAncestor;
+        else return bfdV.distTo(commonAncestor) + bfdW.distTo(commonAncestor);
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w){
-        return -1;
+    public int ancestor(int v, int w) {
+        BreadthFirstDirectedPaths bfdV = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths bfdW = new BreadthFirstDirectedPaths(G, w);
+        int distance = Integer.MAX_VALUE;
+        int commonAncestor = -1;
+        for (int i = 0; i < G.V(); i++) {
+            if (bfdV.hasPathTo(i) && bfdW.hasPathTo(i)) {
+                int local = bfdV.distTo(i) + bfdW.distTo(i);
+                if (local < distance) {
+                    distance = local;
+                    commonAncestor = i;
+                }
+            }
+        }
+        return commonAncestor;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w){
-        return -1;
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+        BreadthFirstDirectedPaths bfdV = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths bfdW = new BreadthFirstDirectedPaths(G, w);
+        int distance = Integer.MAX_VALUE;
+        int commonAncestor = -1;
+        for (int i = 0; i < G.V(); i++) {
+            if (bfdV.hasPathTo(i) && bfdW.hasPathTo(i)) {
+                int local = bfdV.distTo(i) + bfdW.distTo(i);
+                if (local < distance) {
+                    distance = local;
+                    commonAncestor = i;
+                }
+            }
+        }
+        if (commonAncestor == -1) return commonAncestor;
+        else return bfdV.distTo(commonAncestor) + bfdW.distTo(commonAncestor);
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
-        return -1;
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        BreadthFirstDirectedPaths bfdV = new BreadthFirstDirectedPaths(G, v);
+        BreadthFirstDirectedPaths bfdW = new BreadthFirstDirectedPaths(G, w);
+        int distance = Integer.MAX_VALUE;
+        int commonAncestor = -1;
+        for (int i = 0; i < G.V(); i++) {
+            if (bfdV.hasPathTo(i) && bfdW.hasPathTo(i)) {
+                int local = bfdV.distTo(i) + bfdW.distTo(i);
+                if (local < distance) {
+                    distance = local;
+                    commonAncestor = i;
+                }
+            }
+        }
+        return commonAncestor;
     }
 
     // do unit testing of this class
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
